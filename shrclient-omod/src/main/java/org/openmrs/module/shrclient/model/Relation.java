3c32a0f2db35eedbc1e30e93ba5f1673dc79a47d
nehashri <nehashrl@thoughtworks.com>
2017-02-01 14:04:05 +0530

    Neha,Abhilash | shr-1429 | adding json ignore unknown properties for Patient.

--- shrclient-omod/src/main/java/org/openmrs/module/shrclient/model/Relation.java
+++ shrclient-omod/src/main/java/org/openmrs/module/shrclient/model/Relation.java
@@ -2,12 +2,14 @@ package org.openmrs.module.shrclient.model;
 
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Relation {
 
     @JsonProperty("type")
