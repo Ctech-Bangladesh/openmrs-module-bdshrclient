d235c7090425e4a09196c3c9d5b96c1d13adc84d
MD Dubey <mddubey409@gmail.com>
2017-02-07 12:28:30 +0530

    Dubey, Abhilash | SHR-1403 | Merged BahmniSyncRetryTask into BahmniSyncTask. Deleted BahmniSyncRetryTask.

--- shrclient-omod/src/main/java/org/openmrs/module/shrclient/scheduler/tasks/BahmniSyncTask.java
+++ shrclient-omod/src/main/java/org/openmrs/module/shrclient/scheduler/tasks/BahmniSyncTask.java
@@ -14,10 +14,15 @@ public class BahmniSyncTask extends AbstractBahmniSyncTask {
 
     @Override
     protected void executeBahmniTask(PatientPush patientPush, EncounterPush encounterPush, PropertiesReader propertiesReader) {
+        /*
+        * todo: for now this class processes new and failed events both. Failed events should be processed in BahmniSyncRetryTask.
+        * */
         log.debug("SCHEDULED JOB : SHR Patient Sync Task");
         try {
             getFeedClient(OPENMRS_PATIENT_FEED_URI, patientPush, propertiesReader.getMciMaxFailedEvent()).processEvents();
+            getFeedClient(OPENMRS_PATIENT_FEED_URI, patientPush, propertiesReader.getMciMaxFailedEvent()).processFailedEvents();
             getFeedClient(OPENMRS_ENCOUNTER_FEED_URI, encounterPush, propertiesReader.getShrMaxFailedEvent()).processEvents();
+            getFeedClient(OPENMRS_ENCOUNTER_FEED_URI, encounterPush, propertiesReader.getShrMaxFailedEvent()).processFailedEvents();
         } catch (URISyntaxException e) {
             log.error(e.getMessage());
         }
