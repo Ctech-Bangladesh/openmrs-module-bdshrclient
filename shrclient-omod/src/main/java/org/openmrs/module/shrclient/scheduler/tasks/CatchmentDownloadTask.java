6b6a0f4daeb0c98e020639c34562f5acbfe3f179
nehashri <nehashrl@thoughtworks.com>
2017-02-22 14:28:29 +0530

    Neha | shr-1429 | merging Encounter and Patient Catchment tasks.

--- shrclient-omod/src/main/java/org/openmrs/module/shrclient/scheduler/tasks/CatchmentDownloadTask.java
+++ shrclient-omod/src/main/java/org/openmrs/module/shrclient/scheduler/tasks/CatchmentDownloadTask.java
@@ -2,19 +2,21 @@ package org.openmrs.module.shrclient.scheduler.tasks;
 
 import org.apache.log4j.Logger;
 import org.openmrs.module.shrclient.handlers.EncounterPull;
+import org.openmrs.module.shrclient.handlers.PatientPull;
 import org.openmrs.module.shrclient.identity.IdentityStore;
-import org.openmrs.module.shrclient.identity.IdentityUnauthorizedException;
 import org.openmrs.module.shrclient.util.PlatformUtil;
 import org.openmrs.module.shrclient.util.PropertiesReader;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
-public class CatchmentEncounterDownloadTask extends AbstractTask {
-    private static final Logger log = Logger.getLogger(CatchmentEncounterDownloadTask.class);
+public class CatchmentDownloadTask extends AbstractTask {
+    private static final Logger log = Logger.getLogger(CatchmentDownloadTask.class);
 
     @Override
     public void execute() {
         PropertiesReader propertiesReader = PlatformUtil.getPropertiesReader();
         IdentityStore identityStore = PlatformUtil.getIdentityStore();
+        new PatientPull(propertiesReader, identityStore).download();
+        new PatientPull(propertiesReader, identityStore).retry();
         new EncounterPull(propertiesReader, identityStore).download();
         new EncounterPull(propertiesReader, identityStore).retry();
     }
--- shrclient-omod/src/main/java/org/openmrs/module/shrclient/scheduler/tasks/CatchmentPatientDownloadTask.java
+++ shrclient-omod/src/main/java/org/openmrs/module/shrclient/scheduler/tasks/CatchmentPatientDownloadTask.java
@@ -1,10 +1,6 @@
 package org.openmrs.module.shrclient.scheduler.tasks;
 
 import org.apache.log4j.Logger;
-import org.openmrs.module.shrclient.handlers.PatientPull;
-import org.openmrs.module.shrclient.identity.IdentityStore;
-import org.openmrs.module.shrclient.util.PlatformUtil;
-import org.openmrs.module.shrclient.util.PropertiesReader;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
 public class CatchmentPatientDownloadTask extends AbstractTask {
@@ -12,9 +8,7 @@ public class CatchmentPatientDownloadTask extends AbstractTask {
 
     @Override
     public void execute() {
-        PropertiesReader propertiesReader = PlatformUtil.getPropertiesReader();
-        IdentityStore identityStore = PlatformUtil.getIdentityStore();
-        new PatientPull(propertiesReader, identityStore).download();
-        new PatientPull(propertiesReader, identityStore).retry();
+//        Moved to encounter sync task.
+//        TODO: Patient pull to have its own task. And avoid race conditions. 
     }
 }
